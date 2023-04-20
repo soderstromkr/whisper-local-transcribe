dfe967bd5802b388eb4ea63d7d5bc01ed1d37e8f
Kristofer Rolf Söderström <kristofer.soderstrom@gmail.com>
2023-04-20 19:35:51 +0200

    Update run_Windows.bat

--- run_Windows.bat
+++ run_Windows.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Starting...
-call conda activate venv
+call conda activate base
 REM OPTION 2 : (KEEP TEXT WITHIN QUOTES AND CHANGE USERNAME) "C:/Users/user/Anaconda3/condabin/activate.bat"
 call python GUI.py
